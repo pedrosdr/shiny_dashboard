a7ca500ea2bafc078d0d366455aff0800d8a9d8d
Pedro Sartori <pedrosartdr@gmail.com>
2023-08-06 01:04:23 -0300

    initial commit

---
+++ util.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(dplyr)
+library(data.table)
+library(plotly)
+library(ggplot2)
+
+data = read.csv('data.csv') %>%
+  rename(CAMBIO='CÂMBIO') %>% 
+  rename(DIRECAO='DIREÇÃO') %>%
+  rename(CAMERA_DE_RE='CÂMERA.DE.RÉ') %>%
+  rename(DIRECAO_HIDRAULICA='DIREÇÃO.HIDRÁULICA') %>%
+  rename(SENSOR_DE_RE='SENSOR.DE.RÉ') %>%
+  rename(TRAVA_ELETRICA='TRAVA.ELÉTRICA') %>%
+  rename(VIDRO_ELETRICO='VIDRO.ELÉTRICO') %>%
+  rename(AR_CONDICIONADO='AR.CONDICIONADO') %>%
+  rename(AIR_BAG = 'AIR.BAG')
+
+theme_set(theme_bw())
